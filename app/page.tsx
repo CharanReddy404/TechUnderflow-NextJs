76524fa7c13c9f1b0db5f82d9b8d45d7927caf51
CharanReddy404 <Charanlucky6143@gmail.com>
2024-01-11 07:34:16 +0530

    implementing  routing

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className='h1-bold'>Hello Nextjs let&apos;s start</h1>
-      <h2 className='h2-bold'>Hello Nextjs let&apos;s start</h2>
-      <h3 className='h3-bold'>Hello Nextjs let&apos;s start</h3>
-    </div>
-  );
-}
