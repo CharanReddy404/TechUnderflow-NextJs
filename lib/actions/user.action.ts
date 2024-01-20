e7afb04e356963f1d7e007b84a0b57dbb2079dc5
CharanReddy404 <Charanlucky6143@gmail.com>
2024-01-20 06:49:22 +0530

    community page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,6 +5,7 @@ import { connectToDatabase } from '../mongoose';
 import {
   CreateUserParams,
   DeleteUserParams,
+  GetAllUsersParams,
   UpdateUserParams,
 } from './shared.types';
 import { revalidatePath } from 'next/cache';
@@ -15,8 +16,6 @@ export async function getUserById(params: any) {
     connectToDatabase();
 
     const { userId } = params;
-
-    console.log('------------', params);
 
     const user = await User.findOne({ clerkId: userId });
 
@@ -90,3 +89,27 @@ export async function deleteUser(params: DeleteUserParams) {
     throw error;
   }
 }
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    const users = await User.find({}).sort({ createdAt: -1 });
+
+    return { users };
+  } catch (error) {
+    console.log('error');
+    throw error;
+  }
+}
+
+// export async function getAllUsers(params: GetAllUsersParams) {
+//   try {
+//     connectToDatabase();
+//   } catch (error) {
+//     console.log('error');
+//     throw error;
+//   }
+// }
